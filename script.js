89087d0a4cbdd7ca9e294c33cb7dcd2563a06f3e
pik8014 <pierre.konan@mavs.uta.edu>
2025-10-22 22:44:41 -0500

    about section done

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 // *Main page
+let miniImages = document.querySelectorAll(".mini-about-image img");
+
+miniImages.forEach((miniImage) => {
+  miniImage.addEventListener("mouseover", () => {
+    let img = document.querySelector(".image");
+    img.style.width = "100%";
+    img.style.height = "100%";
+    img.src = miniImage.src;
+  });
+});
+let mainImg = document.querySelector(".image");
+mainImg.src = miniImages[0].src;
+mainImg.style.width = "100%";
+mainImg.style.height = "100%";
